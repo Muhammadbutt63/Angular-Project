3276d4606b3aed42116886c4163b33b252809a5f
Muhammadbutt63 <muhammadbutt637@gmail.com>
2023-11-06 17:29:38 +0500

    add

--- Project-Angular/src/app/app-routing.module.ts
+++ Project-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'services', component: AboutUsComponent},
+  {path: 'contact-us', component: AboutUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Project-Angular/src/app/app.module.ts
+++ Project-Angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
