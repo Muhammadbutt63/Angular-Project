06998c3290f135139daa6acb56da9a651a99a810
Muhammadbutt63 <muhammadbutt637@gmail.com>
2023-11-13 20:11:55 +0500

    add

--- Project-Angular/src/app/home/home.component.ts
+++ Project-Angular/src/app/home/home.component.ts
@@ -6,9 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  public showTable:Boolean = true;
   public myName: string = "Dell";
   public myName2: string = "Del";
   public human:any = [
-    
+    {name: "Dell",  age: 25},
+    {name: "Dell2", age: 26},
+    {name: "Dell3", age: 27},
+    {name: "Dell4", age: 28},
+    {name: "Dell5", age: 29},
+    {name: "Dell6", age: 30},
+    {name: "Dell7", age: 31},
+    {name: "Dell8", age: 32},
+    {name: "Dell9", age: 33}
   ]
+
+  public showTableMethod():void{
+    this.showTable = !this.showTable;
+  }
+
+
 }
